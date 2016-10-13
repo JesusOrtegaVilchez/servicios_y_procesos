06979f8f427113a031e039dc98e4d31257c60171
JesusOrtegaVilchez <jesus_a_t_g@hotmail.com>
2016-10-13 14:07:44 +0200

    parents & childs

---
+++ 04_spawn/run.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int spawn(char* program, char** argv)
+{
+    pid_t child_pid;
+    /*Duplicate the process*/
+    child_pid = fork();
+    if(child_pid!=0)
+        /*This is the parent process*/
+        return child_pid;
+    else{
+        /*Now execute PROGRAM, searching for in the path. */ 
+        execvp(program,argv);
+        /*The execvp function return only if an error occurs. */
+        fprintf(stderr, "an error ocurred in execvp\n");
+        abort();
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    /*Spawn a child process running the "ls" command. Ignore the 
+     * returned child process ID. */
+    spawn("ls",argv);
+    printf("done woth main program\n");
+
+    return EXIT_SUCCESS;
+
+}
